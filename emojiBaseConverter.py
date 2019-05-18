d95c9ad4f49744fea9a8e46209d9d2a66273dbba
Lena Hegemann <lenahegemann@Lenas-MacBook-Pro.local>
2019-05-18 13:39:53 +0300

    fix deemoji function

--- emojiBaseConverter.py
+++ emojiBaseConverter.py
@@ -1,6 +1,6 @@
 emojiBase = {}
 
-def emoji():
+def emojify():
 	with open("emoji-data.txt", 'r') as emojifile:
 		counter = 0
 		for line in emojifile:
@@ -10,6 +10,13 @@ def emoji():
 			print('{}, {}'.format(counter, emojiBase[counter]))
 			counter = counter + 1
 
+def safeEmojiLookup():
+	file = open("emojilookup.txt", "x")
+	counter = 0
+	for x in range(0, len(emojiBase)):
+		file.write('{}, {}\n'.format(x, emojiBase[x]))
+	file.close()
+
 def convertToBase(number, base):
 	if number < base:
 		return emojiBase[number]
@@ -17,7 +24,8 @@ def convertToBase(number, base):
 		return convertToBase(number//base, base) + emojiBase[number%base]
 
 
-emoji()
+emojify()
+safeEmojiLookup()
 
 #emojiList = []
 print("aalto university: " + convertToBase(32771, len(emojiBase)))
