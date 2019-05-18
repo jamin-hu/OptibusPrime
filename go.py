da0a39eca0a5cc38e2bf3838403b2e21ab0a80de
Marek Baczynski <marekba@kth.se>
2019-05-18 09:01:34 +0300

    API based line creation in go.py

--- go.py
+++ go.py
@@ -1,4 +1,5 @@
 import json
+import random, requests
 
 primes = []
 
@@ -6,10 +7,14 @@ with open('100kprimes.txt', 'r') as primefile:
 	for line in primefile:
 		primes.append(int(line))
 
+emojilut = []
+with open('emojilut.txt', 'r') as emojifile:
+	for line in emojifile:
+		emojilut.append(line[0])
+
 with open("stops.txt", 'r') as stops:
 	data = json.load(stops)
 	orgstops = data['data']['stops']
-
 
 class Station():
 	def __init__(self, primeid, name, orgid, lat, lon):
@@ -18,6 +23,18 @@ class Station():
 		self.orgid = orgid
 		self.lat = lat
 		self.lon = lon
+
+
+def run_query(query):
+    headers = {'Content-type': 'application/json'}
+    
+    r = requests.post("https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql", 
+                  json={'query': query}, headers=headers)
+    if r.status_code == 200:
+        return r.json()
+    else:
+        raise Exception("Query failed to run by returning code of {}. {}".format(request.status_code, query))
+
 
 hslidToStopObject={}
 nameToPrime={}
@@ -29,6 +46,38 @@ for stop in orgstops:
 	hslidToStopObject[newStation.orgid] = newStation
 	print('{}, {}, {}, {}, {}'.format(newStation.name, newStation.primeid, newStation.orgid, newStation.lat, newStation.lon))
 
+def getLine(number):
+    query_stops_by_bus = '{routes(name: "' + str(number) + '" ) \{shortName longName patterns \{code directionId name stops \{name gtfsId \}\}\}\}'
+    result = run_query(query_stops_by_bus)
+    orgRouteName = result['data']['routes'][0]['patterns'][0]['name']
+    route = result['data']['routes'][0]['patterns'][0]['stops']
+    lineno = 1
+    linetext = ""
+    for stop in route:
+        lineno *= nameToPrime[stop['name']]
+        linetext += stop['name'] + "(" + str(nameToPrime[stop['name']]) +") -> "
+    print('\n')
+    print("Lame original name that isn't helpful: {}".format(orgRouteName))
+    print("Bus line: {}".format(lineno))
+    print('\n')
+    print (linetext)
+
+def getl(code):
+    query_stops_by_bus = '{(id: "' + str(code) + '" ) \{name stops \{name gtfsId \}\}\}'
+    result = run_query(query_stops_by_bus)
+    print (result)
+    orgRouteName = result['data']['pattern']['name']
+    route = result['data']['pattern']['stops']
+    lineno = 1
+    linetext = ""
+    for stop in route:
+        lineno *= nameToPrime[stop['name']]
+        linetext += stop['name'] + "(" + str(nameToPrime[stop['name']]) +") -> "
+    print('\n')
+    print("Lame original name that isn't helpful: {}".format(orgRouteName))
+    print("Bus line: {}".format(lineno))
+    print('\n')
+    print (linetext)
 
 def makeLine(length):
 	lineno = 1
@@ -40,5 +89,3 @@ def makeLine(length):
 	print("Line number: {}".format(lineno))
 	print("Route:")
 	print(linetext)
-
-
